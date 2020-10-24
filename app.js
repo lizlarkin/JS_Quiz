dfe31c04d9af804b61c0071f1a1bdd55309139e2
Liz <lizlarkin@highlandtechnology.com>
2020-10-24 12:21:59 -0700

    added index to manage questions, started end of game logic

--- app.js
+++ app.js
@@ -4,35 +4,60 @@ var timerEl = document.querySelector("#timer");
 var welcomeEl = document.querySelector("#welcomediv");
 var quizEl = document.querySelector("#quizdiv");
 var highscoresEl = document.querySelector("#highscoresdiv");
-
 var questionEl = document.querySelector("#question");
-var choicesEl = document.querySelector("#choices");
+var choices1El = document.querySelector("#choice1");
+var choices2El = document.querySelector("#choice2");
+var choices3El = document.querySelector("#choice3");
 var button = document.querySelector("#button");
+var choices = document.querySelector("li");
+var storeName = document.querySelector("#playerName");
+var storeScore = document.querySelector("#playerScore");
+
 var timerId;
 var score = 0;
+var currentIndex = 0;
 
 // Welcome Screen
 // Hide quiz div and scores div 
 quizEl.style.display = "none";
 highscoresEl.style.display = "none";
+scoresEl.textContent = score;
 
 // Timer that counts down from one minute to zero
 // Lose 10 seconds when answer question wrong
+function countdownTimer() {
+    startTime--;
+    timerEl.textContent = (`${startTime} seconds remain`);
+    if (startTime <= 0) {
+        quizEl.style.display = "none";
+        highscoresEl.style.display = "initial";
+        timerEl.textContent = "Times up!";
+        clearInterval(timerId);
+    }
+}
 
 // Set timer start at 60 seconds
 var startTime = 60; 
 timerEl.textContent = (`${startTime} seconds remain`);
 
 // Define Quiz Questions
-var quizContent = [
-   {title: "Question1?",
-    choices: ["choice1", "choice2", "choice3"] ,
-    answer: "choice3"
-   },
-   {title: "Question2?",
-    choices: ["choice1", "choice2", "choice3"] ,
-    answer: "choice2"
-   }];
+var questionArray = [
+    {
+        question: "What is a for loop?",
+        options: ["A way to iterate code", "A way to avoid rewriting code", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        question: "What number index is bird in this list: cat, turtle, bird, zebra",
+        options: ["1", "2", "4"],
+        answer: "2"
+    },
+    {
+        question: "What might an object contain?",
+        options: ["A method", "An event handler", "All of the above"],
+        answer: "A method"
+    }
+]
 
    
 // Start Quiz
@@ -47,57 +72,63 @@ button.addEventListener("click", function() {
     
     // Show first question and first choices
     quizEl.style.display = "initial";
-    quizEl.textContent = quizContent[0].title + quizContent[0].choices;
-
+    displayQuestion();
+   
 });
 
+function displayQuestion() {
+    questionEl.textContent = questionArray[currentIndex].question;
+    choices1El.textContent = questionArray[currentIndex].options[0];
+    choices2El.textContent = questionArray[currentIndex].options[1];
+    choices3El.textContent = questionArray[currentIndex].options[2];
+}
+
 // Take Quiz
-// Loop through questions and choices
-
-
-    // Change choices content  //change this to for each
-    // for (let i = 0; i < choices1.length; i++) {
-    // var arr1 = choices1[i];        
-    // var li = document.createElement("li");
-    // li.textContent = (arr1);
-    // choicesEl.appendChild(li);
-    // }
-
-
-function countdownTimer() {
-    startTime--;
-    timerEl.textContent = (`${startTime} seconds remain`);
-    if (startTime <= 0) {
-        quizEl.style.display = "none";
-        highscoresEl.style.display = "initial";
-        timerEl.textContent = "Times up!";
+choices.addEventListener("click", function() {
+    if (this.textContent === questionArray[currentIndex].answer) {
+        score++;
+    } else {
+        startTime = startTime - 10;
     }
+    
+    if (currentIndex < questionArray.length - 1 ) {
+        currentIndex++;
+        displayQuestion();
+    } else {
+        endOfGame();
     }
+})
+
+// Score 
+var userName = [];
+var userScore = [];
+var userIndex = 0;
+var userName = 
+
+function endOfGame() {
+    // dsplay end of game div
+    highscoresEl.style.display = "initial";
+    quizEl.style.display = "none";
+
+    // collect user information
+    let userInput = prompt("What is your name?");
+    userName.push(userInput);
+    userScore.push(score);
+
+    // display high scores
+    storeName.textContent = userName;
+    storeScore.textContent = userScore;
+}
 
 
 
-// Scores
+// var player = [
+//     {initials: "",
+//     scores: [] ,
+//     },
+//     {title: "Question2?",
+//      choices: ["choice1", "choice2", "choice3"] ,
+//      answer: "choice2"
+//     }]
 
-var player = [
-    {initials: "",
-     scores: [] ,
-    },
-    {title: "Question2?",
-     choices: ["choice1", "choice2", "choice3"] ,
-     answer: "choice2"
-    }]
-
-// // Add Event Listener to Choices
-// li.addEventListener("click", function()){
-//     // Change Timer
-
-//     // Track Score
-//     if (choices[i] === choices[3]) {
-//         score ++
-//     } else {
-//         timer - 10
-//     }
-    // Change question content
-
-    // Change choices content
 
